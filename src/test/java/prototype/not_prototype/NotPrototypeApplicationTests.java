7ef04bae27d26143a83b58a56258169f6af71b08
mark <meksoichkin@gmail.com>
2019-10-30 09:37:15 +0300

    Added new test suite with AopTestUtils

--- src/test/java/prototype/not_prototype/NotPrototypeApplicationTests.java
+++ src/test/java/prototype/not_prototype/NotPrototypeApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.AopTestUtils;
 import prototype.not_prototype.fun.Prototype;
 import prototype.not_prototype.fun.ShouldBePrototype;
 
@@ -19,12 +20,30 @@ public class NotPrototypeApplicationTests {
 
     @Test
     public void ok() {
-        assert (prototypeFactory.getObject()!= prototypeFactory.getObject());
+        assert (prototypeFactory.getObject() != prototypeFactory.getObject());
     }
 
     @Test
     public void notOk() {
         assert (shouldBePrototypeFactory.getObject() != shouldBePrototypeFactory.getObject());
+    }
+
+    /**
+     * https://github.com/spring-projects/spring-framework/issues/23725#issuecomment-546374463
+     */
+    @Test
+    public void sbrannenRecommendation() {
+        ShouldBePrototype proxy1 = shouldBePrototypeFactory.getObject();
+        ShouldBePrototype proxy2 = shouldBePrototypeFactory.getObject();
+
+        ShouldBePrototype target1 = AopTestUtils.getUltimateTargetObject(proxy1);
+        ShouldBePrototype target2 = AopTestUtils.getUltimateTargetObject(proxy2);
+
+        assert (target1 != target2);
+
+        ShouldBePrototype shouldBeEqualTarget1 = AopTestUtils.getUltimateTargetObject(proxy1);
+        assert (shouldBeEqualTarget1 == proxy1);
+
     }
 
 }
